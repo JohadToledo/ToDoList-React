9025e24b431fed9ddee9e142f154189ebe3f86f5
Johad Toledo <johadtledo@gmail.com>
2022-01-18 19:59:08 +0100

    trying ref hook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
+import Home from "./Components/Home";
 import TodoList from "./Components/TodoList";
 
 function App() {
   return (
     <div className="todo-app">
       <TodoList />
+      <Home />
     </div>
   );
 }
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,23 @@
-import React from 'react'
-import './Home.css'
+// import React from 'react'
+// import './Home.css'
+
+// export default function Home() {
+//     return (
+//         <div>
+//             <img src='https://i.ibb.co/6vdyf8J/39019-completing-taskscompressed.gif' alt='home' />
+//         </div>
+//     )
+// }
+
+import React, { useRef } from "react";
 
 export default function Home() {
-    return (
-        <div>
-            <img src='https://i.ibb.co/6vdyf8J/39019-completing-taskscompressed.gif' alt='home' />
-        </div>
-    )
+  const myContainer = useRef(null);
+  console.log(myContainer.current);
+
+  return (
+    <>
+      <div ref={myContainer}>I can use the DOM with react ref</div>
+    </>
+  );
 }
